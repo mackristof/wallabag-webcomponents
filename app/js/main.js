044cc130733db70baff0f4a5e96e97ed56ea2816
mackristof <christophe@mourette.eu>
2014-12-11 19:37:22 +0100

    add angular framework

---
+++ app/js/main.js
@@ -0,0 +1,54 @@
+/*var toastGroupTemplate = document.querySelector('#toastGroup');
+toastGroupTemplate.showToast = function() {
+  document.querySelector('#toast').show();
+};*/
+
+document.addEventListener('polymer-ready', function() {
+  var navicon = document.getElementById('navicon');
+  var drawerPanel = document.getElementById('drawerPanel');
+  navicon.addEventListener('click', function() {
+    drawerPanel.togglePanel();
+  });
+});
+
+angular.module('WallabagIndexDep', [
+    // Angular official modules.
+    'ngResource',
+    'ngRoute',
+    // Rest api provider
+    'wallabag-restapi'
+]);
+
+
+var WallabagIndexApp = angular.module('WallabagIndexApp', ['WallabagIndexDep']);
+
+WallabagIndexApp
+  .config(['$routeProvider',
+        function ($routeProvider){
+            $routeProvider
+                .when('/:user/unread', {
+                    templateUrl: 'partials/unread.html',
+                    controller: 'UnreadController',
+                    controllerAs: 'ctrl',
+                    resolve: {
+                        unreadURLs: ['$route', 'SearchService', function ($route, SearchService) {
+                           var params = $route.current.params;
+                            console.log('params.user='+ params.user);
+                            return EntryService.unread({'user':params.user}).$promise;
+                        }]
+                    }
+                })
+
+                .otherwise({
+                    templateUrl: 'partials/landing.html',
+                });
+        }
+    ])
+    .controller('UnreadController', function( $scope, unreadURLs ) {
+        var ctrl = {
+            unreadUrlList: unreadURLs
+        };
+        return ctrl;
+
+
+    });
---
+++ app/js/restapi.js
@@ -0,0 +1,27 @@
+angular.module('wallabag-restapi', ['ngResource'])
+
+
+    .factory('EntryService', function($resource) {
+        return $resource('api/u/:user/entries', null, {
+            'getUnreads': {
+                method: 'GET'
+                isArray: true
+            },
+            'addEntry' : {
+                method: 'POST',
+            },
+            'updateEntry' : {
+                method: 'POST',
+                url:    'api/u/:user/entry/:id'
+            },
+            'getEntry' : {
+                method: 'GET',
+                url:    'api/u/:user/entry/:id'
+            },
+            'deleteEntry' : {
+                method: 'DELETE',
+                url: 'api/u/:user'
+            }
+        });
+    })
+;
